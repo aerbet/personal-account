ef6f6540a1c72e49c3283e7dbec93f0df3e48963
Erbol Zhaparov <ae.270718@gmail.com>
2026-02-21 08:28:50 +0600

    feat: add register request and response

---
+++ src/main/java/kg/zhaparov/personal_account/payload/response/RegisterResponse.java
@@ -0,0 +1,54 @@
+package kg.zhaparov.personal_account.payload.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+public class RegisterResponse {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String otp;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+}
